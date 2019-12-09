2f6f848f31af354eeb31e5db65f4185c8bba7f3c
Shiyu Li <timlee0212@outlook.com>
2019-12-09 11:45:56 -0500

    Done.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "unistd.h"
+#include <sys/wait.h>
 
 #define SH_BUF_SIZE 1024
 char *sh_read_line()
@@ -39,6 +41,143 @@ char *sh_read_line()
         }
     }
 }
+
+#define SH_TOK_BUFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
+char **sh_split_line(char *line)
+{
+    int buf_size = SH_TOK_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(sizeof(char*) * buf_size);
+    char *token;
+
+    if(!tokens)
+    {
+        printf("sh: Memory Allocation Error!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SH_TOK_DELIM);
+    while(token!=NULL)
+    {
+        tokens[position] = token;
+        position++;
+
+        if(position>=buf_size)
+        {
+            buf_size += SH_TOK_BUFSIZE;
+            tokens = realloc(tokens, buf_size);
+            if(!tokens)
+            {
+                printf("sh: Memory Allocation Error!\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, SH_TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int sh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if(pid==0) //In Child Process
+    {
+        if(execvp(args[0], args) == -1){
+            perror("sh");
+        }
+    }else if(pid<0){ //Forking Error
+        perror("sh");
+    }else{    //Parent Process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        }while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+//Declaration of builtin functions
+int sh_cd(char **args);
+int sh_help(char **args);
+int sh_exit(char **args);
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+    &sh_cd,
+    &sh_help,
+    &sh_exit
+};
+
+int num_builtins(){
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int sh_cd(char **args)
+{
+    if(args[1] == NULL)
+    {
+        fprintf(stderr, "sh: Expected Argument to \"cd\"\n");
+    }else{
+        if(chdir(args[1])!=0)
+        {
+            perror("sh");
+        }
+    }
+    return 1;
+}
+
+int sh_help(char **args)
+{
+    int i;
+    printf("Simple Shell\n");
+    printf("Type Program Names and Arguments, and hit enter. \n");
+    printf("The following are builtin functions:\n");
+
+    for(i = 0; i < num_builtins(); i++)
+    {
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Use man for information on other programs.\n");
+    return 1;
+}
+
+int sh_exit(char **args)
+{
+    exit(EXIT_SUCCESS);
+    return 0;
+}
+
+int sh_execute(char **args)
+{
+    int i;
+    if(args[0] == NULL){
+        return 1;
+    }
+
+    //Find if it is in the builtin function list
+    for(i=0; i< num_builtins(); i++)
+    {
+        if(strcmp(args[0], builtin_str[i])==0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    //If not, launch the program.
+    return sh_launch(args);
+}
+
 //The main loop has three steps:
 //1. Read, get the command and its args from stdin
 //2. Parse, Split the input line into command and its args.
@@ -53,7 +192,7 @@ void sh_loop()
         printf("> ");
         line = sh_read_line();
         args = sh_split_line(line);
-        status = sh_exexute(args);
+        status = sh_execute(args);
 
         free(line);
         free(args);
